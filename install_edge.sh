0e5d09ee34ae14a4c721aa442be63ed86186a366
maprpartners <jsun@maprtech.com>
2018-04-19 17:22:02 +0000

    gcp

--- install_edge.sh
+++ install_edge.sh
@@ -4,3 +4,5 @@ sed -i s#PermitRootLogin\ no#PermitRootLogin\ yes#g /etc/ssh/sshd_config
 service sshd restart
 echo root:mapr | chpasswd
 yum -y install docker
+systemctl enable docker
+systemctl start docker
