d5f0654543691f34d0a47300c4cde7d23f49a4b0
maprpartners <jsun@maprtech.com>
2018-04-19 16:30:29 +0000

    gcp

--- vpc.sh
+++ vpc.sh
@@ -1,3 +1,6 @@
 gcloud compute networks create maprvpc
+
+subnet=`gcloud compute networks subnets list | grep us-east1 | grep maprvpc | awk '{print $4}'`
+
 gcloud compute firewall-rules create allowmapr --network maprvpc --allow tcp:22,tcp:8443,tcp:9443 --source-ranges 0.0.0.0/0
-gcloud compute firewall-rules create allowmaprint --network maprvpc --allow all --source-ranges 0.0.0.0
+gcloud compute firewall-rules create allowmaprint --network maprvpc --allow all --source-ranges ${subnet}
