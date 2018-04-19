ad49ae322cbfb07f7a1d7fb4ac6fee983f115364
maprpartners <jsun@maprtech.com>
2018-04-19 19:15:29 +0000

    gcp

--- install_edge.sh
+++ install_edge.sh
@@ -6,3 +6,12 @@ echo root:mapr | chpasswd
 yum -y install docker
 systemctl enable docker
 systemctl start docker
+
+echo [MapR_Core] > /etc/yum.repos.d/mapr_core.repo
+echo name = MapR Core Components >> /etc/yum.repos.d/mapr_core.repo
+echo baseurl = http://package.mapr.com/releases/v6.0.1/redhat >> /etc/yum.repos.d/mapr_core.repo
+echo gpgcheck = 1 >> /etc/yum.repos.d/mapr_core.repo
+echo enabled = 1 >> /etc/yum.repos.d/mapr_core.repo
+echo protected = 1 >> /etc/yum.repos.d/mapr_core.repo
+
+yum -y install mapr-client
